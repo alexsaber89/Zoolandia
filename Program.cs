03cc61bbc7f386ed78a3ab5527676c9d4615f26f
Alex Saber <Alex Saber>
2017-01-16 03:07:16 -0600

    log strings with animal attributes

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Zoolandia.Animals;
 
 namespace ConsoleApplication
 {
@@ -6,7 +7,21 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var eschrichtiusRobustus = new EschrichtiusRobustus();
+            eschrichtiusRobustus.name = "Fred";
+            Console.WriteLine($"I have a {eschrichtiusRobustus.commonName} named {eschrichtiusRobustus.name} who {eschrichtiusRobustus.endangered}.");
+
+            var eretmochelysImbricata = new EretmochelysImbricata();
+            eretmochelysImbricata.name = "Donald";
+            Console.WriteLine($"I have a {eretmochelysImbricata.commonName} named {eretmochelysImbricata.name} who {eretmochelysImbricata.endangered}.");
+
+            var dicerosBicornis = new DicerosBicornis();
+            dicerosBicornis.name = "Jim";
+            Console.WriteLine($"I have a {dicerosBicornis.commonName} named {dicerosBicornis.name} who {dicerosBicornis.endangered}.");
+
+            var alligatorMississippiensis = new AlligatorMississippiensis();
+            alligatorMississippiensis.name = "Susan";
+            Console.WriteLine($"I have a {alligatorMississippiensis.commonName} named {alligatorMississippiensis.name} who {alligatorMississippiensis.endangered}.");
         }
     }
 }
